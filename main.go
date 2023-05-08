23bc1e21bd0b7783eb8890dd80fda33b7fe4ce26
Roger.H <rogerstudy@outlook.com>
2023-05-08 14:25:21 +0800

    first import

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+var apiKey string
+
+func main() {
+	fmt.Println("done")
+
+	apiKey = os.Getenv("ApiKey")
+
+	if apiKey == "" {
+		// try from env file
+		if key, err := readConfigFile(); err != nil {
+			fmt.Fprintln(os.Stderr, "ApiKey not set")
+			os.Exit(1)
+		} else {
+			apiKey = key
+		}
+	}
+
+	startServer().Run()
+
+}
+
+func readConfigFile() (string, error) {
+	// look for env file in the same folder
+	viper.SetConfigName("env")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return "", err
+	}
+	return viper.GetString("ApiKey"), nil
+}
